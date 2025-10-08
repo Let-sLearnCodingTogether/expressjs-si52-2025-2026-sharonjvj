6f0537eeae15fd5f0608328dbfa1dfe59061977d
sharonjvj <sharonjadelyn05@gmail.com>
2025-10-08 20:40:23 +0700

    Pertemuan 8 Oktober
    bljr hash(enkrip/dekrip)

--- controller/authController.js
+++ controller/authController.js
@@ -1,4 +1,5 @@
 import UserModel from "../models/userModel.js";
+import { hash } from "../utils/hashUtil.js";
 
 export const register = async (req,res) => {
     try {
@@ -7,10 +8,12 @@ export const register = async (req,res) => {
 
         console.log(registerData);
 
+        const hashPassword = hash(registerData.password)
+
         await UserModel.create({
             username : registerData.username,
-            emai : registerData.email,
-            password : registerData.password
+            email : registerData.email,
+            password : hashPassword
         })
 
         res.status(201).json({
@@ -20,6 +23,47 @@ export const register = async (req,res) => {
     } catch (e) {
         res.status(500).json({
             message : e.message,
+            data : null
+        })
+    }
+}
+
+export const login = async (req,res) => {
+    try {
+        const loginData = req.body
+
+        const user =  await UserModel.findOne({
+            email : loginData.email
+        })
+
+        //jika user tidak ditemukan
+        if(!user){
+            res.status(404).json({
+                message : "User tidak ditemukan",
+                data : null
+            })
+        }
+        
+
+        //membandingkan password yang ada di dalam db dengan request
+        if(user.password == loginData.password){
+                return res.status(200).json({
+                    message : "Login Berhasil",
+                    data : {
+                        username : user.username,
+                        email : user.email,
+                        token : "TOKEN"
+                    }
+                })
+        }
+        return res.status(401).json({
+                    message : "Login Gagal",
+                    data : null
+                })
+
+    } catch (error) {
+        res.status(500).json({
+            message : error.message,
             data : null
         })
     }
